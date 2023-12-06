5faf9b43fa555313d80d8e0bff8e99779fb72d5f
Alexandr <115313122+AlexandrMyName@users.noreply.github.com>
2023-12-06 19:46:59 +0300

    First commit

---
+++ Pages/DownloadApp.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyVoronegWeb5._0.Pages
+{
+    public class DownloadAppModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
